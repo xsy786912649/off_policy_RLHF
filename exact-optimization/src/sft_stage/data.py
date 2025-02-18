e15d5627515617560df14b52f470961fb49adc0d
xsy786912649 <786912649@qq.com>
2025-02-18 17:03:06 -0500

    Update data.py

--- exact-optimization/src/sft_stage/data.py
+++ exact-optimization/src/sft_stage/data.py
@@ -9,7 +9,10 @@ class SFTDataset(BaseDataset):
         if inference:
             prompt = self.get_prompt(sample)
             chosen = self.get_chosen(sample)
-            #assert(chosen.strip() != ""), chosen
+            print(prompt)
+            print(chosen)
+            input()
+            assert(chosen.strip() != ""), chosen
             tokens = self.tokenizer.encode(prompt + chosen)
             chosen_tokens = self.tokenizer.encode(chosen)
 
