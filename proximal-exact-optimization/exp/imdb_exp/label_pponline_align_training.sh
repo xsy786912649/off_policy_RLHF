ae4877d36f6be42af6e2a4f2592eff78310a27fd
xsy786912649 <786912649@qq.com>
2025-03-04 18:26:21 -0500

    update

---
+++ proximal-exact-optimization/exp/imdb_exp/label_pponline_align_training.sh
@@ -0,0 +1,12 @@
+
+deepspeed --include "localhost:$1" \
+          --master_port 1235 src/rm_stage/label_pponline_train.py \
+          --data_path $2 \
+          --split $3 \
+          --model_path $4 \
+          --batch_size 64 \
+          --mode $5 \
+          --max_length 512 \
+          --overwrite \
+          --kernel_inject \
+          --eval_num -1 \
