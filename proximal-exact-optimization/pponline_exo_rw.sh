9a2f2dd088080851a1d5964fc8fe5f0ee4a23c2e
xsy786912649 <786912649@qq.com>
2025-03-05 00:15:27 -0500

    update

--- proximal-exact-optimization/pponline_exo_rw.sh
+++ proximal-exact-optimization/pponline_exo_rw.sh
@@ -1,12 +1,12 @@
 #1.1
 bash exp/imdb_exp/inference_pponline_align_training.sh 2 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
 
-#bash exp/imdb_exp/inference_pponline_align_training.sh 2 imdb/sft:exp/imdb_exp/data/imdb_prefix10 test models/gpt2-large_imdb/sft
+bash exp/imdb_exp/inference_pponline_align_training.sh 2 imdb/sft:exp/imdb_exp/data/imdb_prefix10 test models/gpt2-large_imdb/sft
 
 #1.2
 bash exp/imdb_exp/label_pponline_align_training.sh 2 exp/imdb_exp/data/imdb_prefix10_pponline train models/gpt2-large_imdb/rm label
 
-#bash exp/imdb_exp/label_pponline_align_training.sh 2 exp/imdb_exp/data/imdb_prefix10_pponline test models/gpt2-large_imdb/rm label
+bash exp/imdb_exp/label_pponline_align_training.sh 2 exp/imdb_exp/data/imdb_prefix10_pponline test models/gpt2-large_imdb/rm label
 
 #1.3
 bash exp/imdb_exp/train_pponline.sh 2 gpt2-large models/gpt2-large_imdb/sft exp/imdb_exp/data/imdb_prefix10_pponline_rw "pp-exo-rw" 2 models/gpt2-large_imdb/sft 1
