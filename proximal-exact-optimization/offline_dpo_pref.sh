62144321008e277eaa47e0fef8194b07e3319c8c
xsy786912649 <786912649@qq.com>
2025-03-05 17:23:03 -0500

    Update offline_dpo_pref.sh

--- proximal-exact-optimization/offline_dpo_pref.sh
+++ proximal-exact-optimization/offline_dpo_pref.sh
@@ -20,6 +20,7 @@ bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/align_dpo
 
 #3
 
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
 
 bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
 
