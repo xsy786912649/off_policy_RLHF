0ca8073c89d7a41c0714f6637f858f71f80a2e6c
xsy786912649 <786912649@qq.com>
2025-03-05 17:23:00 -0500

    update

---
+++ proximal-exact-optimization/offline_exo_pref.sh
@@ -0,0 +1,57 @@
+#1
+
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
+
+#bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 test models/gpt2-large_imdb/sft
+
+bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
+
+#bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft test models/gpt2-large_imdb/rm label
+
+bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/sft exp/imdb_exp/data/imdb_prefix10_rw "exo-pref" 2 models/gpt2-large_imdb/sft 1
+
+#2
+
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
+
+bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
+
+bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/align_exo-pref_nc2/ckpt1 exp/imdb_exp/data/imdb_prefix10_rw "exo-pref" 2 models/gpt2-large_imdb/sft 2
+
+#3
+
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
+
+bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
+
+bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/align_exo-pref_nc2/ckpt2 exp/imdb_exp/data/imdb_prefix10_rw "exo-pref" 2 models/gpt2-large_imdb/sft 3
+
+#4
+
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
+
+bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
+
+bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/align_exo-pref_nc2/ckpt3 exp/imdb_exp/data/imdb_prefix10_rw "exo-pref" 2 models/gpt2-large_imdb/sft 4
+
+#5
+
+bash exp/imdb_exp/inference_sft.sh 0 imdb/sft:exp/imdb_exp/data/imdb_prefix10 train models/gpt2-large_imdb/sft
+
+bash exp/imdb_exp/inference_rm.sh 0 exp/imdb_exp/data/imdb_prefix10_sft train models/gpt2-large_imdb/rm label
+
+bash exp/imdb_exp/train_offline.sh 0 gpt2-large models/gpt2-large_imdb/align_exo-pref_nc2/ckpt4 exp/imdb_exp/data/imdb_prefix10_rw "exo-pref" 2 models/gpt2-large_imdb/sft 5
+
+#inference test
+
+bash exp/imdb_exp/inference_align.sh 0 models/gpt2-large_imdb/align_exo-pref_nc2
+
+bash exp/imdb_exp/inference_class.sh 0 exp/imdb_exp/data/imdb_prefix10_infer_res/models/gpt2-large_imdb/align_exo-pref_nc2/ckpt1 test models/gpt2-large_imdb/class eval
+
+bash exp/imdb_exp/inference_class.sh 0 exp/imdb_exp/data/imdb_prefix10_infer_res/models/gpt2-large_imdb/align_exo-pref_nc2/ckpt2 test models/gpt2-large_imdb/class eval
+
+bash exp/imdb_exp/inference_class.sh 0 exp/imdb_exp/data/imdb_prefix10_infer_res/models/gpt2-large_imdb/align_exo-pref_nc2/ckpt3 test models/gpt2-large_imdb/class eval
+
+bash exp/imdb_exp/inference_class.sh 0 exp/imdb_exp/data/imdb_prefix10_infer_res/models/gpt2-large_imdb/align_exo-pref_nc2/ckpt4 test models/gpt2-large_imdb/class eval
+
+bash exp/imdb_exp/inference_class.sh 0 exp/imdb_exp/data/imdb_prefix10_infer_res/models/gpt2-large_imdb/align_exo-pref_nc2/ckpt5 test models/gpt2-large_imdb/class eval
--- proximal-exact-optimization/runn_all.sh
+++ proximal-exact-optimization/runn_all.sh
@@ -1,11 +1,11 @@
-bash offline_dpo_rw.sh
+bash offline_dpo_pref.sh
 
-bash online_dpo_rw.sh
+bash online_dpo_pref.sh
 
-bash pponline_dpo_rw.sh
+bash pponline_dpo_pref.sh
 
-bash offline_exo_rw.sh
+bash offline_exo_pref.sh
 
-bash online_exo_rw.sh
+bash online_exo_pref.sh
 
-bash pponline_exo_rw.sh
+bash pponline_exo_pref.sh
