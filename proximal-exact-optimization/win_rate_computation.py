d6af0bb437dca45345be05245aeefe3991612180
xsy786912649 <786912649@qq.com>
2025-03-06 01:00:45 -0500

    Update win_rate_computation.py

--- proximal-exact-optimization/win_rate_computation.py
+++ proximal-exact-optimization/win_rate_computation.py
@@ -17,15 +17,27 @@ for i in [1,2,3,4,5]:
     data = json.load(open(path1, "r"))
     data2 = json.load(open(path2, "r"))
     assert len(data) == len(data2)
+
+    total_number = len(data)
+
+    win_total = 0
+
     for j in range(len(data)):
         line = data[j]
         line2 = data2[j]
-        print([row[0] for row in line["scores_texts"]])
-        print([row[0] for row in line2["scores_texts"]])
-        input()
+        rewards_sft=[row[0] for row in line["scores_texts"]]
+        rewards_win=[row[0] for row in line2["scores_texts"]]
+
+        win_numer_temp=0
+        for reward000 in rewards_sft:
+            for reward111 in rewards_win:
+                if reward111 >= reward000:
+                    win_numer_temp += 1
+        win_total += win_numer_temp / (len(rewards_sft) * len(rewards_win))
 
     print("--------------------")
     print("Checkpoint_finish",i)
+    print("win_total",win_total/total_number)
     print("--------------------")
 
 
