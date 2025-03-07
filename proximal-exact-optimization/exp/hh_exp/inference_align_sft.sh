c154649edef223567f3446998268ec726e6f5ab9
xsy786912649 <786912649@qq.com>
2025-03-06 23:30:38 -0500

    update

---
+++ proximal-exact-optimization/exp/hh_exp/inference_align_sft.sh
@@ -0,0 +1,26 @@
+# args:
+# - 1: device ids
+# - 2: model_path
+
+
+temp=0.8
+ref_temp=0.8
+
+
+# generate
+model_path=$2
+
+deepspeed --include "localhost:$1" \
+        --master_port 1231 src/align_stage/inference.py \
+                        --split test --model_path $model_path \
+                        --prompt_num 512 \
+                        --return_num 4 \
+                        --seed 42 \
+                        --temp $temp \
+                        --data_name_path hh/sft:exp/hh_exp/data/hh \
+                        --max_length 512 \
+                        --max_new_tokens 500 \
+                        --batch_size 32 \
+                        --kernel_inject
+
+#bash exp/hh_exp/inference_align_sft.sh 0,1,2,3 models/pythia-2.8b_hh/sft
