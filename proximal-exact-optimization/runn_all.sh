fabeaabbfc01d535f49b676b42f5ca46c98f02f5
xsy786912649 <786912649@qq.com>
2025-03-07 01:26:16 -0500

    Update runn_all.sh

--- proximal-exact-optimization/runn_all.sh
+++ proximal-exact-optimization/runn_all.sh
@@ -4,17 +4,26 @@ bash online_dpo_rw.sh
 
 bash pponline_dpo_rw.sh
 
+
+
+
 bash offline_exo_rw.sh
 
 bash online_exo_rw.sh
 
 bash pponline_exo_rw.sh
 
+
+
+
 bash offline_dpo_pref.sh
 
 bash online_dpo_pref.sh
 
 bash pponline_dpo_pref.sh
+
+
+
 
 bash offline_exo_pref.sh
 
