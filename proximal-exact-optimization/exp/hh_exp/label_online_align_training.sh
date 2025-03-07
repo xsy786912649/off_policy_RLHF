7d99e101254d3b669b3cd0c446bf599219b150a4
xsy786912649 <786912649@qq.com>
2025-03-07 15:40:12 -0500

    update

--- proximal-exact-optimization/exp/hh_exp/label_online_align_training.sh
+++ proximal-exact-optimization/exp/hh_exp/label_online_align_training.sh
@@ -3,7 +3,7 @@ deepspeed --include "localhost:$1" \
           --data_path $2 \
           --split $3 \
           --model_path $4 \
-          --batch_size 64 \
+          --batch_size 192 \
           --mode $5 \
           --max_length 512 \
           --overwrite \
