410f0eb1bdc90ddea3e229d38ed36c653e03ddfb
xsy786912649 <786912649@qq.com>
2025-03-07 15:24:24 -0500

    update

--- proximal-exact-optimization/exp/hh_exp/inference_online_align_training.sh
+++ proximal-exact-optimization/exp/hh_exp/inference_online_align_training.sh
@@ -7,6 +7,6 @@ deepspeed --include "localhost:$1" \
                             --max_len 512 \
                             --temp 0.8 \
                             --return_num 2 \
-                            --batch_size 64 \
+                            --batch_size 128 \
 
                             
