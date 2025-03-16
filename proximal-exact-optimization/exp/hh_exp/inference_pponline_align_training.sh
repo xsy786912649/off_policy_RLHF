8a3cc91ad1bd16881fdb167af41a44308da0d073
xsy786912649 <786912649@qq.com>
2025-03-16 02:13:44 -0400

    update

--- proximal-exact-optimization/exp/hh_exp/inference_pponline_align_training.sh
+++ proximal-exact-optimization/exp/hh_exp/inference_pponline_align_training.sh
@@ -2,7 +2,7 @@ deepspeed --include "localhost:$1" \
           --master_port 1235 src/sft_stage/inference_pponline_train.py \
                             --split $3 --model_path $4 \
                             --data_name_path $2 \
-                            --prompt_num -1 \
+                            --prompt_num 22400 \
                             --max_new_tokens 200 \
                             --max_len 512 \
                             --temp 0.8 \
--- proximal-exact-optimization/exp/hh_exp/inference_sft.sh
+++ proximal-exact-optimization/exp/hh_exp/inference_sft.sh
@@ -2,7 +2,7 @@ deepspeed --include "localhost:$1" \
           --master_port 1231 src/sft_stage/inference.py \
                             --split $3 --model_path $4 \
                             --data_name_path $2 \
-                            --prompt_num -1 \
+                            --prompt_num 22400 \
                             --temp 0.8 \
                             --max_len 512 \
                             --max_new_tokens 200 \
