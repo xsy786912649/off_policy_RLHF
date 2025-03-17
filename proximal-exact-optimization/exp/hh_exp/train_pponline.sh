fa92ec231402beac0e47f5e8e767607b6b36c815
xsy786912649 <786912649@qq.com>
2025-03-17 16:55:16 -0400

    update

--- proximal-exact-optimization/exp/hh_exp/train_pponline.sh
+++ proximal-exact-optimization/exp/hh_exp/train_pponline.sh
@@ -15,9 +15,9 @@ tb_path=tb_logs
 dataset_abbr=$( echo $dataset_name | cut -d'/' -f1 )
 
 # general
-#train_bsz * grad_accum * num_gpus = 4 * 8 * 4 = 256
-#step = number / (train_bsz  * num_gpus)= 43200 / ( 4 *8 ) = 1350
-# max_iter_step = 700 
+#train_bsz * grad_accum * num_gpus = 4 * 8 * 4 = 128
+#step = number / (train_bsz  * num_gpus)= 43200 / ( 4 *4 ) = 2700
+# max_iter_step = 1400
 port=1486
 train_bsz=4
 eval_bsz=4
