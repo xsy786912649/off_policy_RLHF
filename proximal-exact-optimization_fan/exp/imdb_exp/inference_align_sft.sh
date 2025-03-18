ade012aa6805e9dc87171ffdceb50cc7ca7bc246
xsy786912649 <786912649@qq.com>
2025-03-17 21:52:45 -0400

    update

---
+++ proximal-exact-optimization_fan/exp/imdb_exp/inference_align_sft.sh
@@ -0,0 +1,26 @@
+# args:
+# - 1: device ids
+# - 2: model_path
+
+
+temp=0.8
+ref_temp=0.8
+
+
+# generate
+model_path=$2
+
+deepspeed --include "localhost:$1" \
+        --master_port 1231 src/align_stage/inference.py \
+                        --split test --model_path $model_path \
+                        --prompt_num 512 \
+                        --return_num 4 \
+                        --seed 42 \
+                        --temp $temp \
+                        --data_name_path imdb/sft:exp/imdb_exp/data/imdb_prefix10 \
+                        --max_length 512 \
+                        --max_new_tokens 500 \
+                        --batch_size 32 \
+                        --kernel_inject
+
+#bash exp/imdb_exp/inference_align_sft.sh 0,1 models/gpt2-large_imdb/sft
---
+++ proximal-exact-optimization_fan/hh_bash/pponline_dpo_pref.sh
@@ -0,0 +1,64 @@
+#1.1
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh train models/pythia-2.8b_hh/sft 1
+
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh test models/pythia-2.8b_hh/sft 0
+
+#1.2
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline train models/pythia-2.8b_hh/rm label
+
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline test models/pythia-2.8b_hh/rm label
+
+#1.3
+bash exp/hh_exp/train_pponline.sh 0,1,2,3 pythia-2.8b models/pythia-2.8b_hh/sft exp/hh_exp/data/hh_pponline_rw "pp-dpo-pref" 2 models/pythia-2.8b_hh/sft 1
+
+#2.1
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh train models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt1 2
+
+#2.2
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline train models/pythia-2.8b_hh/rm label
+
+#2.3
+bash exp/hh_exp/train_pponline.sh 0,1,2,3 pythia-2.8b models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt1 exp/hh_exp/data/hh_pponline_rw "pp-dpo-pref" 2 models/pythia-2.8b_hh/sft 2 
+
+#3.1
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh train models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt2 3
+
+#3.2
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline train models/pythia-2.8b_hh/rm label
+
+#3.3
+bash exp/hh_exp/train_pponline.sh 0,1,2,3 pythia-2.8b models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt2 exp/hh_exp/data/hh_pponline_rw "pp-dpo-pref" 2 models/pythia-2.8b_hh/sft 3 
+
+#4.1
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh train models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt3 4
+
+#4.2
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline train models/pythia-2.8b_hh/rm label
+
+#4.3
+bash exp/hh_exp/train_pponline.sh 0,1,2,3 pythia-2.8b models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt3 exp/hh_exp/data/hh_pponline_rw "pp-dpo-pref" 2 models/pythia-2.8b_hh/sft 4 
+
+#5.1
+bash exp/hh_exp/inference_pponline_align_training.sh 0,1,2,3 hh/sft:exp/hh_exp/data/hh train models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt4 5
+
+#5.2
+bash exp/hh_exp/label_pponline_align_training.sh 0,1,2,3 exp/hh_exp/data/hh_pponline train models/pythia-2.8b_hh/rm label
+
+#5.3
+bash exp/hh_exp/train_pponline.sh 0,1,2,3 pythia-2.8b models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt4 exp/hh_exp/data/hh_pponline_rw "pp-dpo-pref" 2 models/pythia-2.8b_hh/sft 5
+
+#inference test
+
+bash exp/hh_exp/inference_align.sh 0,1,2,3 models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2
+
+bash exp/hh_exp/inference_rm_test.sh 0,1,2,3 exp/hh_exp/data/hh_infer_res/models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt1 test models/pythia-2.8b_hh/rm eval
+
+bash exp/hh_exp/inference_rm_test.sh 0,1,2,3 exp/hh_exp/data/hh_infer_res/models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt2 test models/pythia-2.8b_hh/rm eval
+
+bash exp/hh_exp/inference_rm_test.sh 0,1,2,3 exp/hh_exp/data/hh_infer_res/models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt3 test models/pythia-2.8b_hh/rm eval
+
+bash exp/hh_exp/inference_rm_test.sh 0,1,2,3 exp/hh_exp/data/hh_infer_res/models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt4 test models/pythia-2.8b_hh/rm eval
+
+bash exp/hh_exp/inference_rm_test.sh 0,1,2,3 exp/hh_exp/data/hh_infer_res/models/pythia-2.8b_hh/align_online_pp-dpo-pref_nc2/ckpt5 test models/pythia-2.8b_hh/rm eval
+
+python hh_bash/win_rate_computation.py online_pp-dpo-pref
