ee7ce79bcb91788403ced8b85cd8aa96f5a089e2
xsy786912649 <786912649@qq.com>
2025-04-09 23:58:54 -0400

    update

--- proximal-exact-optimization_fan/exp/hh_exp_8b/label_pponline_align_training.sh
+++ proximal-exact-optimization_fan/exp/hh_exp_8b/label_pponline_align_training.sh
@@ -3,7 +3,7 @@ deepspeed --include "localhost:$1" \
           --data_path $2 \
           --split $3 \
           --model_path $4 \
-          --batch_size 128 \
+          --batch_size 64 \
           --mode $5 \
           --max_length 512 \
           --overwrite \
