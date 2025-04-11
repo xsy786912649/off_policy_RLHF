ad4fa52f438b7994a55b010fbb09c781efb78f74
xsy786912649 <786912649@qq.com>
2025-04-10 22:43:02 -0400

    update

---
+++ proximal-exact-optimization_fan/test_hh.sh
@@ -0,0 +1,15 @@
+python hh_bash/win_rate_computation.py dpo-pref
+python hh_bash/win_rate_computation.py online_dpo-pref
+python hh_bash/win_rate_computation.py online_pp-dpo-pref
+
+python hh_bash/win_rate_computation.py dpo-rw
+python hh_bash/win_rate_computation.py online_dpo-rw
+python hh_bash/win_rate_computation.py online_pp-dpo-rw
+
+python hh_bash/win_rate_computation.py exo-pref
+python hh_bash/win_rate_computation.py online_exo-pref
+python hh_bash/win_rate_computation.py online_pp-exo-pref
+
+python hh_bash/win_rate_computation.py exo-rw
+python hh_bash/win_rate_computation.py online_exo-rw
+python hh_bash/win_rate_computation.py online_pp-exo-rw
