74456a6bbc16751e987df0e613c2f48fa3976b5a
xsy786912649 <786912649@qq.com>
2025-04-12 23:15:56 -0400

    udpate

---
+++ proximal-exact-optimization_fan/exp/tldr_exp/inference_online_align_training.sh
@@ -0,0 +1,13 @@
+deepspeed --include "localhost:$1" \
+          --master_port 1234 src/sft_stage/inference_online_train.py \
+                            --split $3 --model_path $4 \
+                            --data_name_path $2 \
+                            --iteration $5 \
+                            --prompt_num -1 \
+                            --max_new_tokens 200 \
+                            --max_len 512 \
+                            --temp 0.8 \
+                            --return_num 2 \
+                            --batch_size 256 \
+
+                            
