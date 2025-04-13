c5d1c97d532d9e0a88270e8b507e398d7b7a35a5
xsy786912649 <786912649@qq.com>
2025-04-13 00:14:22 -0400

    Create test_tldr.sh

---
+++ proximal-exact-optimization_fan/test_tldr.sh
@@ -0,0 +1,15 @@
+python tldr_bash/win_rate_computation.py dpo-pref
+python tldr_bash/win_rate_computation.py online_dpo-pref
+python tldr_bash/win_rate_computation.py online_pp-dpo-pref
+
+python tldr_bash/win_rate_computation.py dpo-rw
+python tldr_bash/win_rate_computation.py online_dpo-rw
+python tldr_bash/win_rate_computation.py online_pp-dpo-rw
+
+python tldr_bash/win_rate_computation.py exo-pref
+python tldr_bash/win_rate_computation.py online_exo-pref
+python tldr_bash/win_rate_computation.py online_pp-exo-pref
+
+python tldr_bash/win_rate_computation.py exo-rw
+python tldr_bash/win_rate_computation.py online_exo-rw
+python tldr_bash/win_rate_computation.py online_pp-exo-rw
